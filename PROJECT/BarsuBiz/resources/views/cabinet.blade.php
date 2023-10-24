1d83bd9be1a0a78893f7bf58dce9517930969a4c
MaxOSPHPMen <147160709+MaxOSPHPMen@users.noreply.github.com>
2023-10-24 19:18:36 +0300

    ff

--- PROJECT/BarsuBiz/resources/views/cabinet.blade.php
+++ PROJECT/BarsuBiz/resources/views/cabinet.blade.php
@@ -20,12 +20,59 @@
   <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
   <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
   <script src="assets/vendor/php-email-form/validate.js"></script>
+  <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
+  <link href="{{asset('assets/img/favicon.png')}}" rel="icon">
+  <link href="{{asset('assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="{{asset('https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i')}}" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="{{asset('assets/vendor/aos/aos.css')}}" rel="stylesheet">
+  <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{asset('assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+  <link href="{{asset('assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+  <link href="{{asset('assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+  <link href="{{asset('assets/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
+  <link href="{{asset('assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
   
 </head>
 <body>
 
-    <script src="assets/js/main.js"></script>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary shadow">
+<header id="header">
+    <div class="container-fluid d-flex align-items-center justify-content-between hh">
+
+      <h1 class="logo"><a href="#hero">BarsuBiz</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Дом</a></li>
+          <li><a class="nav-link scrollto" href="#about">О нас</a></li>
+          <li><a class="nav-link scrollto" href="#services">Наши сервиси</a></li>
+          <li><a class="nav-link scrollto" href="#team">Разработчики</a></li>
+          <li><a class="nav-link scrollto" href="#footer">Контакты</a></li>
+          <li>
+          <form method="post" action="{{ route('logout') }}">
+            @csrf
+            <button class="getstarted scrollto" type="submit">Выйти</button>
+          </form>
+           
+          </li>
+          <!-- <li><a class="getstarted scrollto" href="#about">Get Started</a></li> -->
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+    <script src="assets/js/main.js"></script> 
+    <script src="assets/js/main_cab.js"></script> 
+    <!-- <nav class="navbar navbar-expand-lg bg-dark shadow">
         <div class="container d-flex">
           <a class="navbar-brand" href="#">BarsuBiz</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Переключатель навигации">
@@ -38,12 +85,12 @@
             </form>
           </div>
         </div>
-      </nav>
+      </nav> -->
       @auth
       <div class="container">
         
 
-        <div class="row">
+        <div class="row mt-3">
             <div class="col-md-5">
               <h1>Личный кабинет</h1>
                 <div class="card">
