84637dfdebd06abef0d834e6a087d986e7d10c1c
vladwdw <57443656+vladwdw@users.noreply.github.com>
2023-10-31 23:12:41 +0300

    Revert "Merge branch 'main' of https://github.com/vladwdw/BarsuBiz"
    This reverts commit 38b823008a5b088302a87af86bf843b9b8198c36, reversing
    changes made to d1048baf6dd0d14c8ca2541b758bac9cf15618d5.

--- PROJECT/BarsuBiz/routes/web.php
+++ PROJECT/BarsuBiz/routes/web.php
@@ -26,7 +26,7 @@ use Illuminate\Http\Request;
 Route::get('/', function () {
     return view('forms/index');
 })->name('home');
-
+Route::post('/submit-word/{name}/{id}',[MainController::class, 'form_word'])->name('form_word');
 
 Route::get('/login',[MainController::class, 'login'])->name('login');
 Route::get('/register',[MainController::class, 'registerPage'])->name('registerPage');
@@ -34,8 +34,6 @@ Route::post('/submit-register', [RegisterController::class,'register']);
 Route::post('/login',[AuthController::class,'login'])->name('login');
 Route::post('/logout',[AuthController::class,'logout'])->name('logout');
 Route::middleware(['auth'])->group(function () {
-Route::post('/submit-word/{name}/{id}',[MainController::class, 'form1_word'])->name('form1_word');
-Route::get('/submit-pdf/{name}/{id}',[MainController::class, 'form1_pdf'])->name('form1_pdf');
 Route::get('/cabinet',[MainController::class, 'cabinet'])->name('cabinet');
 Route::get('/form1',[MainController::class, 'form1'])->name('form1');
 Route::get('/form11/{name}/{id}',[MainController::class, 'form11'])->name('form11');
@@ -49,7 +47,7 @@ Route::post('/submit-form3', [FormController3::class, 'store']);
 Route::post('/submit-form4', [FormController4::class, 'store']);
 Route::post('/submit-form5', [FormController5::class, 'store']);
 Route::post('/form11/{name}/{id}',[StoreController1::class, 'form11_update'])->name('form11_update');
-
+Route::post('/form11/{name}/{id}/delete',[StoreController1::class, 'form11_delete'])->name('form11_delete');
 });
 Route::get('/css/{filename}', function ($filename) {
     return response()->file(public_path('css/' . $filename));
