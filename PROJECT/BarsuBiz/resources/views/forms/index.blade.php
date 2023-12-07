773430c7f10f453055fd7a76a476dea6eab55ff3
MaxOSPHPMen <147160709+MaxOSPHPMen@users.noreply.github.com>
2023-12-07 22:04:44 +0300

    Update index.blade.php

--- PROJECT/BarsuBiz/resources/views/forms/index.blade.php
+++ PROJECT/BarsuBiz/resources/views/forms/index.blade.php
@@ -643,7 +643,7 @@
               <div class="pic"><img src="assets/img/kalko.png" class="img-fluid" alt=""></div>
               <div class="member-info">
                 <h4>Калько Алексей Игоревич</h4>
-                <span>СЕО, Продакшн</span>
+                <span>Личный водитель</span>
                 <div class="social">
                   <a href=""><i class="bi bi-twitter"></i></a>
                   <a href=""><i class="bi bi-facebook"></i></a>
@@ -659,7 +659,7 @@
               <div class="pic"><img src="assets/img/shah.jpg" class="img-fluid" alt=""></div>
               <div class="member-info">
                 <h4>Шах Александр Васильевич</h4>
-                <span>Product Manager</span>
+                <span>Спонсор</span>
                 <div class="social">
                   <a href=""><i class="bi bi-twitter"></i></a>
                   <a href=""><i class="bi bi-facebook"></i></a>
@@ -670,27 +670,13 @@
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
-            <div class="member">
-              <div class="pic"><img src="assets/img/team/team-3.jpg" class="img-fluid" alt=""></div>
-              <div class="member-info">
-                <h4>Горбач Юлия Валерьевна</h4>
-                <span>CTO</span>
-                <div class="social">
-                  <a href=""><i class="bi bi-twitter"></i></a>
-                  <a href=""><i class="bi bi-facebook"></i></a>
-                  <a href=""><i class="bi bi-instagram"></i></a>
-                  <a href=""><i class="bi bi-linkedin"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
+          
 
         </div>
 
       </div>
-    </section> -->
-
+    </section> 
+-->
     <!-- ======= Contact Section ======= -->
     <!-- <section id="contact" class="contact">
       <div class="container" data-aos="fade-up"">
